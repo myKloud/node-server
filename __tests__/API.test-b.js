6112fafc578a974a3f6c7df203dfa29ef4debaba
user name <email@domain.com>
2022-01-13 13:31:42 +0200

    delete test files

--- __tests__/API.test-b.js
+++
@@ -1,9 +0,0 @@
-describe("Test if the Jest work", () => {
-    test("should  output 2 ", () => {
-      expect(1 * 2).toBe(2);
-    });
-  
-    it("should be 1 = 1 ", () => {
-      expect(1).toBe(1);
-    });
-  });
